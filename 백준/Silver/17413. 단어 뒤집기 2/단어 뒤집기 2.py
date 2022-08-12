9eb1f7ef955e0e22154614add1900dcaf506f713
juhyeon_son <72910402+juhyeon042@users.noreply.github.com>
2022-08-12 20:35:32 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 1168 ms, Memory: 33188 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,21 @@
+S = input()+' '
+S = list(S)
+stack=[]
+i = 0
+while i<len(S):
+    if S[i]=='<':
+        if stack:
+            while stack:
+                print(stack.pop(),end='')
+        ii = S.index('>')
+        print(*S[i:S.index('>')+1],sep='',end='')
+        del S[i:S.index('>')+1]
+    elif S[i] != ' ' and S[i] != '<' and S[i] != '>':
+        stack.append(S[i])
+        i+=1
+    elif S[i] == ' ':
+        if stack:
+            while stack:
+                print(stack.pop(),end='')
+            print(' ',end='')
+        i+=1
