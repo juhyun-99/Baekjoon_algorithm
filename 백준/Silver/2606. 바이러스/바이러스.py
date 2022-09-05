149484c8dbf5f4548ceb8e69dad8ef00c3d4d037
juhyeon_son <72910402+juhyeon042@users.noreply.github.com>
2022-09-05 20:14:56 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,19 @@
+N = int(input())
+graph = [[]*i for i in range(N+1)]
+
+M = int(input())
+for _ in range(M):
+    v1,v2 = map(int,input().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+visit = [0]*(N+1)
+def dfs(graph,v,visit):
+    visit[v] = 1
+
+    for i in graph[v]:
+        if visit[i] ==0:
+            dfs(graph,i,visit)
+
+dfs(graph,1, visit)
+print(sum(visit)-1)
+  
