156c9a8d33a4efb9f5e3249d1cbcdab7b6b47850
juhyeon_son <72910402+juhyeon042@users.noreply.github.com>
2022-09-10 15:49:24 +0900

    Update 게임.py

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -2,7 +2,7 @@ import sys;input = sys.stdin.readline
 X,Y = map(int,input().split())
 Z = Y*100//X
 
-if Z>=99:
+if Z>=99: #승률이 99가 넘어가면 절대 승률을100를 달성할 수 없기 때문(한판이라도 졌음 or 다 이김)
     print(-1)
     exit()
 
