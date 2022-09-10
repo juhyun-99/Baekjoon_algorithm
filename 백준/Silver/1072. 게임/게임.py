4abe53de00125f7ff0502742197d7b7f13bc17d6
juhyeon_son <72910402+juhyeon042@users.noreply.github.com>
2022-09-10 15:17:46 +0900

    [Silver III] Title: 게임, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -1,19 +1,21 @@
 import sys;input = sys.stdin.readline
 X,Y = map(int,input().split())
-Z = (Y*100)//X
+Z = Y*100//X
 
-end = X
+if Z>=99:
+    print(-1)
+    exit()
+
+end = 1000000000
 start = 1
-ans = sys.maxsize
+ans = 0
 while start<=end:
     mid = (start+end)//2
-    if ((Y+mid)*100//(X+mid)) <= Z:
+    if ((Y+mid)*100)//(X+mid) <= Z:
         start = mid+1
     else:
-        ans = min(ans,mid)
+        ans = mid
         end = mid -1
 
-if ans == sys.maxsize:
-    print(-1)
-else: print(ans)
+print(ans)
 
