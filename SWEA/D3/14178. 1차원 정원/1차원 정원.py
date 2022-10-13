0d6200cd45aa326c0686f833d5d604866395a77d
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-10-13 14:34:03 +0900

    [D3] Title: 1차원 정원, Time: 1,136 ms, Memory: 63,796 KB -BaekjoonHub

---
+++ SWEA/D3/14178. 1차원 정원/1차원 정원.py
@@ -0,0 +1,10 @@
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N, D = map(int,input().split())
+    D += (D+1)
+    ans = N//D
+    if N%D > 0:
+        ans += 1
+    print(f"#{test_case} {ans}")
