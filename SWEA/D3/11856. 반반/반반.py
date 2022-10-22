d4c50de7e889487ed023a536d6ac4bbb277387f0
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-10-22 21:15:28 +0900

    [D3] Title: 반반, Time: 92 ms, Memory: 45,696 KB -BaekjoonHub

---
+++ SWEA/D3/11856. 반반/반반.py
@@ -0,0 +1,14 @@
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for tc in range(1, T + 1):
+    s = input()
+    ss = {}
+    ans = "Yes"
+    for i in s:
+        ss[i] = ss.get(i, 0) + 1
+    for i in ss.values():
+        if i != 2:
+            ans = "No"
+
+    print(f"#{tc} {ans}")
