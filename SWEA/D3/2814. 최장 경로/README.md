2b0c10a7990b762362a9cf2c986e6f408259f8ec
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-10-28 23:48:48 +0900

    [D3] Title: 최장 경로, Time: 151 ms, Memory: 60,756 KB -BaekjoonHub

---
+++ SWEA/D3/2814. 최장 경로/README.md
@@ -0,0 +1,11 @@
+# [D3] 최장 경로 - 2814 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GOPPaAeMDFAXB) 
+
+### 성능 요약
+
+메모리: 60,756 KB, 시간: 151 ms, 코드길이: 517 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -0,0 +1,27 @@
+
+T = int(input())
+def dfs(i, num):
+    global s
+    visit[i] = 1
+    s = max(num, s)
+    for j in arr[i]:
+        if visit[j] == 0:
+            dfs(j, num + 1)
+            visit[j] = 0
+
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+
+    arr = [[] for _ in range(N + 1)]
+    for i in range(M):
+        x, y = map(int, input().split())
+        arr[x].append(y)
+        arr[y].append(x)
+
+    s = 0
+    for i in range(1, N + 1):
+        visit = [0] * (N + 1)
+        dfs(i, 1)
+
+
+    print(f'#{tc} {s}')
