dbb1b299d03869150742d3aea8318bc4620fbe21
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-10-29 16:52:13 +0900

    [Silver II] Title: 침투, Time: 916 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.py
+++ 백준/Silver/13565. 침투/침투.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 import sys
 sys.setrecursionlimit(10**6)
 M, N = map(int,input().split())
@@ -24,8 +26,28 @@ def dfs(x, y):
             if visit[nx][ny] == 0 and arr[nx][ny] == '0':
                 dfs(nx, ny)
 
+def bfs(x, y):
+    global ans
+    q = deque()
+    q.append((x, y))
+    visit[x][y] = 1
+
+    while q:
+        x, y = q.popleft()
+        if x == (M - 1):
+            ans = 'YES'
+            return
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < M and 0 <= ny < N:
+                if visit[nx][ny] == 0 and arr[nx][ny] == '0':
+                    q.append((nx, ny))
+                    visit[nx][ny] = 1
+
+
 for i in range(N):
     if arr[0][i] == '0' and visit[0][i] == 0:
-        dfs(0, i)
+        bfs(0, i)
 
 print(ans)
