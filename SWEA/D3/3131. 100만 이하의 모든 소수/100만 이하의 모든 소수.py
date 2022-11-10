576f1080c439f8a5cb11443fba270c431eb7d366
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-11-10 16:32:35 +0900

    [D3] Title: 100만 이하의 모든 소수, Time: 1,272 ms, Memory: 62,900 KB -BaekjoonHub

---
+++ SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.py
@@ -0,0 +1,14 @@
+import math
+
+def sosu(n):
+    if n <= 1:
+        return False
+    else:
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+for i in range(1, 1000000):
+    if sosu(i):
+        print(i, end = ' ')
