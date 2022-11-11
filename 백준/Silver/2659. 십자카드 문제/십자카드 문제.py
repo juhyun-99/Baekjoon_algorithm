edd1906db5bd048b7c5148bb207ab4b79d381ed7
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-11-12 01:47:17 +0900

    [Silver III] Title: 십자카드 문제, Time: 156 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.py
@@ -1,5 +1,18 @@
+
+ll = []
+#print(arr)
+l = list(map(int, input().split()))
+for i in range(4):
+    tmp = ''
+    for j in range(4):
+        tmp += str(l[(i + j) % 4])
+    if tmp[0] != '0':
+        ll.append(int(tmp))
+ll.sort()
+#print(ll)
+
 arr = []
-for i in range(1111, 10000):
+for i in range(1111, ll[0]):
     s = list(str(i))
     if '0' in s:
         continue
@@ -13,16 +26,4 @@ for i in range(1111, 10000):
     idx = int(ss[0])
     if idx not in arr:
         arr.append(idx)
-
-ll = []
-#print(arr)
-l = list(map(int, input().split()))
-for i in range(4):
-    tmp = ''
-    for j in range(4):
-        tmp += str(l[(i + j) % 4])
-    if tmp[0] != '0':
-        ll.append(int(tmp))
-ll.sort()
-#print(ll)
-print(arr.index(ll[0]) + 1)
+print(len(arr) + 1)
