72e1f0b1ffeaee849f35447a72a9921e94e2a9f9
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-11-12 15:10:12 +0900

    [Gold V] Title: 토마토, Time: 1016 ms, Memory: 237212 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+M, N, H = map(int,input().split())
+tomato = [[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
+visit = [[[0] * M for _ in range(N)]for _ in range(H)]
+day = [[[0] * M for _ in range(N)]for _ in range(H)]
+q = deque([])
+cnt = 0
+dx = [1,-1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
+
+for i in range(H):
+    for j in range(N):
+        for k in range(M):
+            if tomato[i][j][k] == 1:
+                q.append((i, j, k))
+                cnt += 1
+            elif tomato[i][j][k] == -1:
+                cnt += 1
+ans = 0
+while q:
+    z, x, y = q.popleft()
+    visit[z][x][y] = 1
+    for i in range(6):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        nz = z + dz[i]
+        if (0 <= nx < N and 0 <= ny < M and 0 <= nz < H) and visit[nz][nx][ny] == 0 and tomato[nz][nx][ny] == 0:
+            visit[nz][nx][ny] = 1
+            tomato[nz][nx][ny] = 1
+            day[nz][nx][ny] = day[z][x][y] + 1
+            ans = max(ans, day[nz][nx][ny])
+            cnt += 1
+            q.append((nz,nx,ny))
+
+if cnt != (M * N * H):
+    print(-1)
+else:
+    print(ans)
