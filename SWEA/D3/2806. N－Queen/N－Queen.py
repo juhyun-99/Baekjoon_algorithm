72843367b6808f894bf13052c82d1c7a69d1f23f
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-11-14 19:06:45 +0900

    [D3] Title: N-Queen, Time: 230 ms, Memory: 61,448 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -0,0 +1,24 @@
+def possible(x):
+    for i in range(x):
+        if queen[i] == queen[x] or abs(x - i) == abs(queen[x] - queen[i]):
+            return False
+    return True
+
+def q(x):
+    global ans
+    if x == n:
+        ans += 1
+        return
+    for i in range(n):
+        queen[x] = i
+        if possible(x):
+            q(x + 1)
+
+t = int(input())
+
+for tc in range(1, t+ 1):
+    n = int(input())
+    queen = [0] * n
+    ans = 0
+    q(0)
+    print(f'#{tc} {ans}')
