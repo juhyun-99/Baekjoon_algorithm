c880dfc36fcfb5ab2e7d51037999a55e43eba711
juhyun <72910402+juhyun-99@users.noreply.github.com>
2022-11-19 14:35:59 +0900

    [Gold II] Title: 문제집, Time: 244 ms, Memory: 39304 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.py
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -1,5 +1,5 @@
 import sys
-from queue import PriorityQueue
+import heapq
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
@@ -13,16 +13,16 @@ for _ in range(M):
     arr[a].append(b)
     num[b] += 1
 
-q = PriorityQueue()
+q = []
 
 for i in range(1, N + 1):
     if num[i] == 0:
-        q.put(i)
+        heapq.heappush(q, i)
 
-while not q.empty():
-    v = q.get()
+while q:
+    v = heapq.heappop(q)
     print(v, end=' ')
     for j in arr[v]:
         num[j] -= 1
         if num[j] == 0:
-            q.put(j)
+            heapq.heappush(q, j)
