f90e0b5ec44712078450d4868b45041a434db23f
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-01-11 17:50:34 +0900

    [Bronze I] Title: 암호 키, Time: 7276 ms, Memory: 72576 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,19 @@
+import math
+sosu = []
+for i in range(2, 1000000): #100만 이하의 모든 소수 구하기
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j ==0:
+            break
+    sosu.append(i)
+
+
+n = int(input())
+for _ in range(n): #입력받은 숫자와 100만 이하의 소수로 나누어 떨어지는지 계산
+    ans = "YES"
+    s = int(input())
+    for num in sosu:
+        if s % num == 0:
+            ans = "NO"
+            break
+    print(ans)
+
