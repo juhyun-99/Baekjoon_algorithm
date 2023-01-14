e4e1a1be5c3bf95b077d50e036c14495579f2489
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-01-15 00:39:53 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arr[] = new int[10];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+            if(sum >= 100){
+                if(Math.abs(sum - 100) > Math.abs(sum - arr[i] - 100)){
+                    sum -= arr[i];
+                }
+                break;
+            }
+        }
+        System.out.println(sum);
+    }
+}
