cd6087c3140c29158da59e4f55a28dbb1105857a
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-01-18 00:05:59 +0900

    [Silver V] Title: 소수 찾기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,22 @@
-import math
-n = int(input())
-arr = list(map(int,input().split()))
-cnt = 0
-def f(num):
-    if num==1:
-        return False
-    for k in range(2,int(math.sqrt(num)+1)):
-        if num%k == 0:
-            return False
-    return True
+import sys
+input = sys.stdin.readline
 
+n = int(input())
+arr = list(map(int, input().split()))
+
+sosu = [True] * 1001
+sosu[1] = False
+
+for i in range(2, int(1000 ** 0.5) + 1):
+    if sosu[i] == False:
+        continue
+    for j in range(i * i, 1001, i):
+        sosu[j] = False
+
+# print(sosu)
+cnt = 0
 for i in arr:
-    if f(i)==True:
-        cnt+=1
+    if sosu[i]:
+        cnt += 1
 
 print(cnt)
