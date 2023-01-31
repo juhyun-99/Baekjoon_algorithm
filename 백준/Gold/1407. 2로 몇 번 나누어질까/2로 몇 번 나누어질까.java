fc2e92f7426e946d9f61ef326c0d9449a6c2777f
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-01-31 14:20:21 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 116 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+
+    static long cnt(long num){
+        if (num == 1) return 1;
+        if (num != 0 && num % 2 == 0) return cnt(num/2) * 2 + num/2;
+        if (num !=1 && num % 2 == 1) return cnt(num - 1) + 1;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(cnt(b) - cnt(a - 1));
+
+    }
+}
