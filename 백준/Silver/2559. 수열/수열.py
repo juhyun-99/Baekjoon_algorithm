0614c0bb069f2c12e37eb96b2e2a02ec034cdcb0
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-02-01 13:55:51 +0900

    [Silver III] Title: 수열, Time: 100 ms, Memory: 40292 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,18 +1,11 @@
-import sys
-input = sys.stdin.readline
+n,k = map(int,input().split())
+arr = [0] + list(map(int,input().split()))
 
-N, K = map(int, input().split())
-arr = tuple(map(int, input().split()))
-
-max_sum = float('-inf')
-start = 0
-cur_sum = 0
-
-for end, val in enumerate(arr):
-    cur_sum += val
-    if end - start + 1 == K:
-        max_sum = max(max_sum, cur_sum)
-        cur_sum -= arr[start]
-        start+=1
-
-print(max_sum)
+s = [0] * (n + 1)
+for j in range(1, k + 1):
+    s[k] += arr[j]
+ans = s[k]
+for i in range(k + 1, n + 1):
+    s[i] = s[i - 1] + arr[i] - arr[i - k]
+    ans = max(ans, s[i])
+print(ans)
