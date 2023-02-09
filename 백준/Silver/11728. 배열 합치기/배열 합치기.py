20d65400aa62c65fd3207e36d8c18e85633084ae
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-02-09 16:43:47 +0900

    [Silver V] Title: 배열 합치기, Time: 1544 ms, Memory: 337748 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,8 +1,21 @@
 import sys
-input = sys.stdin.readline
-N,M = map(int,input().split())
+input= sys.stdin.readline
+
+n,m = map(int,input().split())
 arr1 = list(map(int,input().split()))
 arr2 = list(map(int,input().split()))
-arr3 = arr1+arr2
-arr3.sort()
-print(*arr3,sep=' ')
+num1,num2 = 0,0
+while num1 < n and num2 < m:
+    if arr1[num1] <= arr2[num2]:
+        print(arr1[num1],end =' ')
+        num1 += 1
+
+    else:
+        print(arr2[num2],end = ' ')
+        num2 += 1
+
+if num1 != n:
+    print(*arr1[num1 : ],sep=' ')
+else:
+    print(*arr2[num2:], sep=' ')
+
