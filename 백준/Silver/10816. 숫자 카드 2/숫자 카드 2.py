1720aaa31742e9019c8aaa049b212fe0153d8758
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-02-28 23:56:23 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 932 ms, Memory: 221996 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,15 +1,48 @@
 import sys
-from bisect import bisect_left, bisect_right
 
-N = int(input())
-arr = list(map(int,sys.stdin.readline().split()))
-arr.sort()
-M = int(input())
-targets = list(map(int,sys.stdin.readline().split()))
+input = sys.stdin.readline
 
-for target in targets:
-    num = bisect_right(arr,target) - bisect_left(arr,target)
-    if num > 0:
-        print(num,end=' ')
+n = int(input())
+arr1 = sorted(list(map(int, input().split())))
+m = int(input())
+arr2 = list(map(int, input().split()))
+
+for num in arr2:
+    tf = False
+    start = 0
+    end = n - 1
+    right = 1e19
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr1[mid] == num:
+            right = mid
+            start = mid + 1
+            tf = True
+
+        elif arr1[mid] > num:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+
+    start = 0
+    end = n - 1
+    left = 0
+    while start <= end:
+        mid = (start + end) // 2
+
+        if arr1[mid] == num:
+            left = mid
+            end = mid - 1
+            tf = True
+
+        elif arr1[mid] > num:
+            end = mid - 1
+
+        else:
+            start = mid + 1
+    if tf:
+        print(right - left + 1, end=" ")
     else:
-        print(0,end = ' ')
+        print(0, end=" ")
