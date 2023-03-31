f474084a00ca1bea1f59a57ba11ffce8d9e50fc7
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-03-31 09:55:47 +0900

    [Gold V] Title: 평범한 배낭, Time: 456 ms, Memory: 194320 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,6 +1,5 @@
 N, K = map(int, input().split())
 arr = [list(map(int,input().split())) for _ in range(N)]
-#arr[i][0] = 무게 , arr[i][1] = 가치
 
 l = [[0] * (K + 1) for _ in range(N + 1)]
 
@@ -10,8 +9,6 @@ for i in range(1, N + 1):
             l[i][j] = l[i - 1][j]
         else:
             l[i][j] = max(l[i - 1][j], l[i - 1][j - arr[i - 1][0]] + arr[i - 1][1])
-
-# print(*l, sep = '\n')
 
 tmp = 0
 for i in l:
