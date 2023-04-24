c5e1bc4e62365ec26319b961172ecf137fd5fd32
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-04-24 17:07:10 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,21 +1,22 @@
-N = int(input())
-graph = [[]*i for i in range(N+1)]
+import sys
+input = sys.stdin.readline
+n = int(input())
+graph = [[]*i for i in range(n+1)]
 
-M = int(input())
-for _ in range(M):
-    v1,v2 = map(int,input().split())
+m = int(input())
+for _ in range(m):
+    v1,v2 = map(int,input().rstrip().split())
     graph[v1].append(v2)
     graph[v2].append(v1)
 
-    visit = [0]*(N+1)
+    visit = [0]*(n + 1)
 
-def dfs(graph,v):
+def dfs(graph, v):
     visit[v] = 1
 
     for i in graph[v]:
-        if visit[i] ==0:
-            dfs(graph,i)
+        if visit[i] == 0:
+            dfs(graph, i)
 
-dfs(graph,1)
-print(sum(visit)-1)
-  
+dfs(graph, 1)
+print(sum(visit) - 1)
