95a0b7b4958b7ff468b2777dc137daa795516631
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-05-19 01:31:37 +0900

    [D3] Title: 수의 새로운 연산, Time: 375 ms, Memory: 61,420 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
@@ -4,6 +4,19 @@ for i in range(2, 400):
     arr.append(arr[i - 1] + i)
 
 t = int(input())
+
+def retXY(num):
+    dic = {}
+    x = 1
+    y = num
+    for i in range(arr[num] - num + 1, arr[num] + 1):
+        dic[(x,y)] = i
+        x += 1
+        y -= 1
+    return dic
+
+
+
 for tc in range(1, t + 1):
     p, q = map(int,input().split())
     pp = 0
@@ -18,21 +31,9 @@ for tc in range(1, t + 1):
             qq = i
             break
 
-    dic_p = {}
-    x = 1
-    y = pp
-    for i in range(arr[pp] - pp + 1, arr[pp] + 1):
-        dic_p[(x,y)] = i
-        x += 1
-        y -= 1
-    x = 1
-    y = qq
-    dic_q ={}
-
-    for i in range(arr[qq] - qq + 1, arr[qq] + 1):
-        dic_q[(x,y)] = i
-        x += 1
-        y -= 1
+    dic_p = retXY(pp)
+    dic_q = retXY(qq)
+    
     x1, y1 = 0,0
     x2, y2 = 0,0
 
@@ -48,14 +49,8 @@ for tc in range(1, t + 1):
 
     x = x1 + x2
     y = y1 + y2
-
-    ans_y = 1
-    ans_x = x + (y - 1)
-
-    ans_dic = {}
-    for i in range(arr[ans_x] - ans_x + 1, arr[ans_x] + 1):
-        ans_dic[(ans_y, ans_x)] = i
-        ans_y += 1
-        ans_x -= 1
+    
+    number = x + (y - 1)
+    ans_dic = retXY(number)
 
     print(f'#{tc} {ans_dic[(x,y)]}')
