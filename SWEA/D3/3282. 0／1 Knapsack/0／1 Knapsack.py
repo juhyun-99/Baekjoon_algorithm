54f36c435e90109ddd09f2de077ef046e4f90459
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-05-19 19:42:14 +0900

    [D3] Title: 0/1 Knapsack, Time: 190 ms, Memory: 62,684 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,21 +1,23 @@
-T = int(input())
+t = int(input())
 
-for tc in range(1 , T + 1):
-    N, K = map(int, input().split())
-    arr = [list(map(int,input().split())) for _ in range(N)]
-    dp = [[0] * (K + 1) for _ in range(N + 1)]
+for tc in range(1, t + 1):
+    n, k = map(int, input().split())  # 개수, 부피
+    stuff = [0]  # v 부피, c 가치
+    for _ in range(n):
+        stuff.append(list(map(int, input().split())))
 
-    for i in range(1, N + 1):
-        for j in range(1, K + 1):
-            if j < arr[i-1][0]:
-                dp[i][j] = dp[i-1][j]
+    arr = [[0] * (k + 1) for _ in range(n + 1)]
+    ans = 0
+
+    for i in range(1, n + 1):
+        v, c = stuff[i]
+        for j in range(1, k + 1):
+            if j < v:
+                arr[i][j] = arr[i - 1][j]
+
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - arr[i - 1][0]] + arr[i - 1][1])
+                arr[i][j] = max(arr[i - 1][j], arr[i - 1][j - v] + c)
 
-    #print(*dp, sep = '\n')
-    tmp = 0
-    for i in dp:
-        tmp = max(tmp, max(i))
-    print(f'#{tc} {tmp}')
+        ans = max(max(arr[i]), ans)
 
-
+    print(f'#{tc} {ans}')
