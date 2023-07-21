63be2c9997846bae242f153bb22b573e58d55c6a
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-07-21 22:42:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,43 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int [] arr = new int [9];
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-        }
-        int idx1 = 0,idx2 = 0;
-        loop:
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if(sum - (arr[i] + arr[j]) == 100){
-                    idx1 = i;
-                    idx2 = j;
-                    break loop;
-                }
-            }
-        }
-
-        int [] ans = new int[7];
-        int j = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if(i != idx1 && i != idx2){
-                ans[j] = arr[i];
-                j ++;
-            }
-        }
-
-        Arrays.sort(ans);
-        for (int i = 0; i < ans.length; i++) {
-            System.out.println(ans[i]);
-        }
-
-
-    }
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int arr[] = new int[9];
+		int sum = 0;
+		for(int i = 0; i<9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		int index1 = 0;
+		int index2 = 0;
+		for(int i = 0; i<8; i++) {
+			for(int j = i + 1; j< 9; j++) {
+				if(sum - arr[i] - arr[j] == 100) {
+					index1 = i;
+					index2 = j;
+				}
+			}
+		}
+		
+		int cnt = 0;
+		int ans[] = new int[7];
+		for(int i = 0; i<9; i++) {
+			if (i == index1 || i == index2) {
+				continue;
+			}
+			ans[cnt] = arr[i];
+			cnt += 1;
+		}
+		
+		Arrays.sort(ans);
+		
+		for(int i = 0; i<ans.length; i++) {
+			System.out.println(ans[i]);
+		}
+	}
 }
