98d466d19880bcaad5268af1b12ff33ba80eb8c4
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-07-21 22:12:59 +0900

    [Bronze III] Title: 통학의 신, Time: 80 ms, Memory: 11760 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.java
+++ 백준/Bronze/17945. 통학의 신/통학의 신.java
@@ -1,18 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        //-1000~1000
-        List<Integer> arr = new ArrayList<Integer>();
-        int a =sc.nextInt(), b = sc.nextInt();
-        for(int x = -1000; x <= 1000; x++){
-            if(x*x + 2*x*a + b == 0){
-                arr.add(x);
-            }
-        }
-        Collections.sort(arr);
-        for(int i = 0; i < arr.size(); i++ ){
-            System.out.printf("%d ",arr.get(i));
-        }
-    }
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(bf.readLine(), " ");
+		
+		int a = Integer.parseInt(stk.nextToken());
+		int b = Integer.parseInt(stk.nextToken());
+		
+		
+		for(int x = -1000; x < 1000; x++) {
+			
+			if(x*x + 2*a*x + b == 0) {
+				System.out.printf("%d ", x);
+			}
+		}
+		
+	}
 }
