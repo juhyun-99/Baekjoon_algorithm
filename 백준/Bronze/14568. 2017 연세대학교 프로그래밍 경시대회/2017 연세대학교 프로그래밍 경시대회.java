f858a9cc0bb70b24deb18292879e1450f1d2bcc3
juhyun <72910402+juhyun-99@users.noreply.github.com>
2023-07-23 19:00:45 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 84 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,25 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.*;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        for (int i = 1; i < n; i++) { //택희
-            if(i % 2 == 1) continue;
-            for (int j = 1; j < n; j++) { //영훈
-                for (int k = 0; k < n; k++) { //남규
-                    if(i + j + k != n) continue;
-                    if(k < j + 2) continue;
-
-                    cnt++;
-                }
-            }
-        }
-        System.out.println(cnt);
-
-    }
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		/*
+		 * 남는 사탕 없어야함.
+		 * 남규 > 영훈 + 2
+		 * 0개 받는 사람 없음
+		 * 택희 % 2 != 1
+		 * */
+		
+		int n = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for(int i = 1; i < n - 1; i ++) { //택희
+			if(i % 2 == 1) {
+				continue;
+			}
+			for(int j = 1; j < n - 1 ; j ++) { //남규
+				for(int k = 1; k < n - 1; k++) { //영훈
+					if(j < k + 2) {
+						continue;
+					}
+					
+					if(i + j + k == n) {
+						sum += 1;
+					}
+					
+				}
+			}
+		}
+		System.out.println(sum);
+	}
 }
