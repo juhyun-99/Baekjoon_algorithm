857a06762a8c821329bb8824176928d820185750
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-01-04 19:16:41 +0900

    [Bronze II] Title: Farm, Time: 80 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.java
+++ 백준/Bronze/16283. Farm/Farm.java
@@ -1,19 +1,30 @@
-import java.util.*;
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.zip.InflaterInputStream;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a =sc.nextInt(),b =sc.nextInt(),n = sc.nextInt(),w = sc.nextInt();
-        int cnt = 0;
-        int num1 = 0 , num2 = 0;
-        for (int i = 1; i < n; i++) {
-            if((i*a + (n - i) * b) == w ) {
-                cnt++;
-                num1 = i;
-                num2 = n - i;
-            }
-        }
-        if(cnt != 1) System.out.println(-1);
-        else System.out.printf("%d %d",num1,num2);
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(stk.nextToken()); //양
+		int b = Integer.parseInt(stk.nextToken()); //염소
+		int n = Integer.parseInt(stk.nextToken()); //양 + 염소
+		int w = Integer.parseInt(stk.nextToken()); //하루 동안 소비한 사료의 양
 
-    }
+		int cnt = 0;
+		int ans = 0;
+		for(int i = 1; i <= n - 1; i ++) {
+			if((a*i + b*(n-i)) == w) {
+				cnt += 1;
+				ans = i;
+			}
+		}
+
+		if(cnt >= 2 || cnt == 0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.printf("%d %d", ans, n - ans);
+		}
+	}
 }
