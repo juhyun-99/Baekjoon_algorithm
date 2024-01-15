f29a2e1be959380c0e7f8464aae644840223715b
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-01-15 12:39:08 +0900

    [Gold IV] Title: 2로 몇 번 나누어질까, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
+++ 백준/Gold/1407. 2로 몇 번 나누어질까/2로 몇 번 나누어질까.java
@@ -1,15 +1,19 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        System.out.println(cnt(b) - cnt(a - 1));
+    }
+
     static long cnt(long num){
         if (num == 1) return 1;
         if (num != 0 && num % 2 == 0) return cnt(num/2) * 2 + num/2;
-        if (num !=1 && num % 2 == 1) return cnt(num - 1) + 1;
+        if (num % 2 == 1) return cnt(num - 1) + 1;
         return 0;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextLong();
-        long b = sc.nextLong();
-        System.out.println(cnt(b) - cnt(a - 1));
     }
 }
