b8e028c9a7457b87819d7caafae2b47294da89a0
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-01 00:00:03 +0900

    [Silver V] Title: 숫자 카드, Time: 1040 ms, Memory: 141812 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,56 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st;
-	    StringBuilder sb = new StringBuilder();
-	    
-	    int n = Integer.parseInt(br.readLine());
-	    int[] num = new int[n];
-	    st = new StringTokenizer(br.readLine());
-	    for (int i = 0; i < n; i++) {
-	    	num[i] = Integer.parseInt(st.nextToken());
-	    }
-	    int m = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-	    int[] num2 = new int[m];
-	    st = new StringTokenizer(br.readLine());
-	    for (int i = 0; i < m; i++) {
-	    	num2[i] = Integer.parseInt(st.nextToken());
-	    }
-	    
-	    Arrays.sort(num);
-	    int[] ans = new int[m];
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
 
-	    for (int i = 0; i < m; i++) {
-			int start = 0;
-			int end = n - 1;
-			while(start <= end) {
-				int mid = (start + end) / 2;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-				if(num2[i] == num[mid]) {
-					ans[i] = 1;
-					break;
-				}else if(num2[i] < num[mid]) {
-					end = mid - 1;
-				}else if(num2[i] > num[mid]) {
-					start = mid + 1;
-				}
-			}
-			
-		}
-	    for (int i = 0; i < m; i++) {
-			sb.append(ans[i] + " ");
-		}
-	    System.out.println(sb);
-	    
-	}
+        Arrays.sort(arr);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            int start = 0;
+            int end = n - 1;
+            boolean check = false;
+            while(start <= end){
+                int mid = (start + end) / 2;
+                if(num == arr[mid]){
+                    sb.append(1 + " ");
+                    check = true;
+                    break;
+                }else if(num < arr[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+            if(!check) sb.append(0 + " ");
+        }
+        System.out.println(sb);
+    }
 }
