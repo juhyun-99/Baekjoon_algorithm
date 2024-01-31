ce4c04e794761d2e936c1444e81536c3eb056e7c
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-01-31 19:45:12 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 1560 ms, Memory: 147232 KB -BaekjoonHub

--- 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.py
@@ -1,28 +1,25 @@
-import math
-
-n = int(input())
-s = input()
+import sys
+n = int(sys.stdin.readline().strip())
+s = sys.stdin.readline().strip()
 right = [0] * n
+
 if s[-1] == 'E':
     right[-1] = 1
+
 for i in range(n - 2, -1, -1):
     if s[i] == 'E':
         right[i] = right[i + 1] + 1
     else:
         right[i] = right[i + 1]
 
-w, h = 0, 0
-ans = 0
+w, h, ans = 0, 0, 0
+
 for i in range(n):
     if s[i] == 'W':
         w += 1
-
     elif s[i] == 'H':
         h += w
-
-    elif s[i] =='E':
+    elif s[i] == 'E':
         ans += h * (2**(right[i] - 1) - 1)
 
-# print(w)
-# print(h)
-print(ans%1000000007)
+print(ans % 1000000007)
