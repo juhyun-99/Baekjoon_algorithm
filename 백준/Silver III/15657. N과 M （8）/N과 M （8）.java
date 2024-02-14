656144e8fdc5d54a9db1cbe236d32bb3b9fa6c57
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-15 00:47:44 +0900

    [Silver III] Title: N과 M (8), Time: 140 ms, Memory: 22120 KB -BaekjoonHub

---
+++ 백준/Silver III/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] num;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        num = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        recursive(0, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void recursive(int cur, int start){
+        if(cur == m){
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            arr[cur] = num[i];
+            recursive(cur + 1, i);
+        }
+    }
+}
