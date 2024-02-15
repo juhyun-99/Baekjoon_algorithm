fa8b95e7d5c1d058490c74bbdd10612c44e65693
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-16 01:45:00 +0900

    [Platinum V] Title: Do You Know Your ABCs?, Time: 180 ms, Memory: 24240 KB -BaekjoonHub

--- 백준/Platinum V/21818. Do You Know Your ABCs？/Do You Know Your ABCs？.java
+++ 백준/Platinum V/21818. Do You Know Your ABCs？/Do You Know Your ABCs？.java
@@ -1,83 +1,87 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
+
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int t = Integer.parseInt(br.readLine());
-		for (int test = 0; test < t; test++) {
-			int n = Integer.parseInt(br.readLine());
-			long[] arr = new long[n];
+    static int ans;
+    static int[] sel;
+    static int[] arr;
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
 
-			HashSet<Long> set = new HashSet<>(); 
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
-			}
-			
-			for (int i = 0; i < n; i++) {
-				set.add(arr[i]);
-				for (int j = 0; j < n; j++) {
-					if(arr[j] > arr[i])
-						set.add(arr[j] - arr[i]);
-				}
-			}
-			
-			Iterator<Long> iterator = set.iterator();
-			List<Long> list = new ArrayList<>();
-			while(iterator.hasNext()) {
-				list.add(iterator.next());
-			}
-			//System.out.println("뽑은거" + list.toString());
-			
-			int cnt = 0;
-			for (int a = 0; a < list.size(); a++) {
-				for (int b = 0; b <list.size(); b++) {
-					for (int c = 0; c < list.size(); c++) {
-						if(list.get(a) <= list.get(b) && list.get(a) <= list.get(c) && list.get(b) <= list.get(c)) {
-							long tmpA = list.get(a);
-							long tmpB = list.get(b);
-							long tmpC = list.get(c);
-							
-							long[] tmp = {tmpA, tmpB, tmpC, tmpA + tmpB, tmpB + tmpC, tmpA + tmpC, tmpA + tmpB + tmpC};
-							//System.out.println(Arrays.toString(tmp));
-							int count = 0;
-							for (int i = 0; i < n; i++) {
-								for (int j = 0; j < 7; j++) {
-									if(arr[i] == tmp[j]) {
-										count++;
-										break;
-									}
-								}
-							}
-							if(count == n) {
-//								System.out.println(Arrays.toString(arr));
-								//System.out.println(Arrays.toString(tmp));
-								cnt++;
-							}
-						}
-					}
-				}
-			}
-			if(test == t - 1) {				
-				sb.append(cnt);
-			}else {
-				sb.append(cnt + "\n");
-			}
-		}
-		System.out.println(sb);
-	}
+        for (int test = 0; test < t; test++) {
+            n = Integer.parseInt(br.readLine());
+            ans = 0;
+            sel = new int[3];
+            arr = new int[n];
+
+            st = new StringTokenizer(br.readLine());
+
+            TreeSet<Integer> set = new TreeSet<>();
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                set.add(arr[i]);
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if(i == j) continue;
+                    if(arr[i] > arr[j]) set.add(arr[i] - arr[j]);
+                }
+            }
+
+            Integer[] list = set.toArray(new Integer[set.size()]);
+            Arrays.sort(list);
+//            System.out.println("arr : " + Arrays.toString(arr));
+//            System.out.println("list : " + Arrays.toString(list));
+            recursive(0, 0, list);
+            sb.append(ans + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void recursive(int cur, int start, Integer[] list) {
+        if(cur == 3){
+            if(check()){
+//                System.out.println(Arrays.toString(sel));
+                ans++;
+            }
+            return;
+        }
+
+        for (int i = start; i < list.length; i++) {
+            sel[cur] = list[i];
+            recursive(cur + 1, i, list);
+        }
+    }
+
+    public static boolean check(){
+        int[] checkList = {sel[0], sel[1], sel[2],
+                sel[0] + sel[1], sel[1] + sel[2], sel[0] + sel[2],
+                sel[0] + sel[1] + sel[2]};
+
+        boolean[] visit = new boolean[n];
+        int cnt = 0;
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < n; j++) {
+                if(!visit[j] && checkList[i] == arr[j]){
+                    visit[j] = true;
+                    cnt++;
+                }
+            }
+        }
+        if(cnt == n) return true;
+        return false;
+
+    }
 }
