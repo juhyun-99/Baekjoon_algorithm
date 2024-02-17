f1e3a24b25d3758ddc7046d640e22d5b579f0609
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-17 22:53:33 +0900

    [Gold IV] Title: N-Queen, Time: 2568 ms, Memory: 12228 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,40 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, ans = 0;
-	static boolean[] visitCol, visitLeft, visitRight;
- 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		n = Integer.parseInt(br.readLine());
-		visitLeft =  new boolean[2 * n - 1];
-		visitRight = new boolean[2 * n - 1];
-		visitCol = new boolean[n];
-		recursive(0);
-		System.out.println(ans);
-		
-	}
-	private static void recursive(int num) {
-		if(num == n) {
-			ans++;
-			return;
-		}
-		
-		for (int i = 0; i < n; i++) {
-			if(visitLeft[num + i]) continue;
-			if(visitRight[num - i  + (n - 1)]) continue;
-			if(visitCol[i]) continue;
-			visitLeft[num + i] = true;
-			visitRight[num - i + (n-1)] = true;
-			visitCol[i] = true;
-			recursive(num + 1);
-			visitLeft[num + i] = false;
-			visitRight[num - i + (n-1)] = false;
-			visitCol[i] = false;
-		}
-	}
+    static int n;
+    static boolean[] col, rightDown, rightUp;
+    static int cnt = 0;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        col = new boolean[n];
+        rightDown = new boolean[2 * n - 1];
+        rightUp = new boolean[2 * n - 1];
+        recursive(0);
+        System.out.println(cnt);
+
+    }
+
+    private static void recursive(int cur) {
+        if(cur == n){
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(col[i]) continue;
+            if(rightDown[cur - i + (n - 1)]) continue;
+            if(rightUp[cur + i]) continue;
+
+            col[i] = true;
+            rightDown[cur - i + (n - 1)] = true;
+            rightUp[cur + i] = true;
+
+            recursive(cur + 1);
+
+            col[i] = false;
+            rightDown[cur - i + (n - 1)] = false;
+            rightUp[cur + i] = false;
+        }
+    }
+
+
 }
