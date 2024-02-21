a960ad5580a8263174138f520b0de676f55cbe26
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-22 00:38:08 +0900

    [Gold IV] Title: 다이어트, Time: 108 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -1,83 +1,89 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
-import java.math.BigInteger;
-
+class Point{
+    int x;
+    int y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 public class Main {
-	static int N;
-	static List<String> ansArr = new ArrayList<>();
-	static List<String> selFood = new ArrayList<>();
-	static int food[][], target[], tmp;
-	static boolean visit[], tf;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		
-		food = new int[N][5];
-		target = new int[4];
-		tf = false;
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < 4; i++) {
-			target[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 5; j++) {
-				food[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		tmp = Integer.MAX_VALUE;
-		
-		recursive(0,0,0,0,0,0,0);
-		
-		if(tf) {	
-			System.out.println(tmp);
-			Collections.sort(ansArr);
-			System.out.println(ansArr.get(0));
-		}else {
-			System.out.println(-1);
-		}
-		
-	}
+    static int n;
+    static int[][] arr;
+    static int[] target, current;
+    static int ans = Integer.MAX_VALUE;
+    static ArrayList<Integer> list = new ArrayList<>();
+    static ArrayList<String> ansList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        target = new int[4];
 
-	private static void recursive(int dan, int ji, int tan, int bi, int price, int foods, int cnt) {
-		
-		
-		if(cnt == N) {
-			if(dan < target[0] || ji < target[1] || tan < target[2] || bi < target[3] || price > tmp) {
-				return;
-			}
-			
-			if(foods == 0) return;
-			if(price <= tmp) {
-				tf = true;
-				if(price < tmp) {
-					ansArr.clear();
-					tmp = price;
-				}
-				String s = "";
-				for (int i = 0; i < selFood.size(); i++) {
-					s+=selFood.get(i);
-				}
-				ansArr.add(s);
-			}
-			return;
-		}
-		
-		selFood.add((cnt + 1) + " ");
-		recursive(dan +food[cnt][0], ji + food[cnt][1], tan + food[cnt][2], bi + food[cnt][3], price + food[cnt][4], foods + 1, cnt + 1);
-		selFood.remove((cnt + 1) + " ");
-		recursive(dan, ji , tan, bi, price, foods, cnt + 1);
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
+        }
 
-	
+        arr = new int[n][5];
+        current = new int[5];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        recursive(0, 0);
+        if(ans == Integer.MAX_VALUE){
+            System.out.println(-1);
+        }else{
+            Collections.sort(ansList);
+            System.out.println(ans);
+            System.out.println(ansList.get(0));
+        }
+    }
+
+    private static void recursive(int cur, int cnt) {
+        if(cur == n){
+            if(cnt == 0) return;
+            for (int i = 0; i < 4; i++) {
+                if(current[i] < target[i]) return;
+            }
+            if(current[4] <= ans){
+                if(current[4] < ans){
+                    ansList.clear();
+                }
+                ans = current[4];
+                String tmp = "";
+                for (int i = 0; i < list.size(); i++) {
+                    tmp += list.get(i) + " ";
+                }
+                ansList.add(tmp);
+            }
+            return;
+        }
+
+        //넣는다
+        for (int i = 0; i < 5; i++) {
+            current[i] += arr[cur][i];
+        }
+        list.add(cur + 1);
+        recursive(cur + 1, cnt + 1);
+
+        list.remove(list.size() - 1);
+
+        //안 넣는다
+        for (int i = 0; i < 5; i++) {
+            current[i] -= arr[cur][i];
+        }
+
+        recursive(cur + 1, cnt);
+    }
 }
