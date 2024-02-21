be201736e27143518d552cc8ca52f89ee2f45d3c
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-21 23:34:35 +0900

    [Silver II] Title: 부분수열의 합, Time: 88 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,51 +1,43 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int s;
+    static int n, s;
     static int[] arr;
-    static int cnt;
-    static StringBuilder sb = new StringBuilder();
+    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         s = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        cnt = 0;
+        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
-
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 1; i <= n; i++) {
-            recursive(0, 0, i, 0);
-        }
-        System.out.println(cnt);
+        
+        recursive(0, 0, 0);
+        System.out.println(ans);
 
 
     }
 
-    private static void recursive(int cur, int start, int end, int sum) {
-        if(cur == end){
-            if(sum == s){
-                cnt++;
+    private static void recursive(int cur, int cnt, int total) {
+        if(cur == n){
+            if(cnt == 0) return;
+            if(total == s){
+                ans++;
             }
             return;
         }
 
-        for (int i = start; i < n; i++) {
-            recursive(cur + 1, i + 1, end, sum + arr[i]);
-        }
+        //안 넣은거
+        recursive(cur + 1, cnt, total);
+
+        //넣은거
+        recursive(cur + 1, cnt + 1, total + arr[cur]);
     }
 }
