62c8228043a243b6069a4b538c7dd2875a43e937
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-02-22 22:06:11 +0900

    [Silver III] Title: 퇴사, Time: 76 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,39 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
+class Work{
+	int time;
+	int money;
+	Work(int time, int money){
+		this.time = time;
+		this.money = money;
+	}
+}
 public class Main {
-	static int n;
-	static int arr[][], ans;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		
-		arr = new int[n][2];
-		for (int i = 0; i < n; i++) {
+		int n = Integer.parseInt(br.readLine());
+		Work[] works = new Work[n + 1];
+		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			works[i] = new Work(t, p);
 		}
-		ans = Integer.MIN_VALUE;
-		recursive(0, 0);
+		int ans = 0;
+		int[] dp = new int[n + 2];
+
+		for (int i = 1; i <= n ; i++) {
+			if(i + works[i].time<= n + 1) {
+				dp[i] = dp[i] + works[i].money;
+				ans = Math.max(dp[i], ans);
+				for (int j = i + works[i].time; j <= n + 1 ; j++) {					
+					dp[j] = Math.max(dp[j], dp[i]);
+				}
+			}
+			//System.out.println(Arrays.toString(dp));
+		}
 		System.out.println(ans);
-	}
-	
-	private static void recursive(int day, int price) {
-		if(day == n) {
-			ans = Math.max(ans, price);
-			return;
-		}
-		
-		if(day > n) {
-			return;
-		}
-		
-		recursive(day + arr[day][0], price + arr[day][1]);
-		recursive(day + 1, price);
 	}
 }
