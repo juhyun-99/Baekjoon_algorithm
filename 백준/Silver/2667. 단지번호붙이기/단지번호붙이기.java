ab1c7db4d61ac78ebe72d23e8f12d706ccb9711e
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-03-21 12:36:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 84 ms, Memory: 11660 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,64 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-	static int n, cnt = 0, sum = 0;
-	static char[][] arr;
-	static boolean[][] visit;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {-1,0,1,0};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		List<Integer> list = new ArrayList<>();
-		n = Integer.parseInt(br.readLine());
-		arr = new char[n][n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = br.readLine().toCharArray();
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(arr[i][j] == '1' ) {
-					arr[i][j] ='0';
-					list.add(bfs(i,j));
-					cnt+=1;
-				}
-			}
-		}
-		sb.append(cnt + "\n");
-		
-		Collections.sort(list);
-		for (int i = 0; i < list.size(); i++) {
-			sb.append(list.get(i) + "\n");
-		}
-		System.out.println(sb);
-	}
-	
-	private static int bfs(int x, int y) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {x,y});
-		int sum = 0;
-		while(!q.isEmpty()) {
-			int[] tmp = q.poll();
-			sum++;
-			for (int i = 0; i < 4; i++) {
-				int nx = tmp[0] + dx[i];
-				int ny = tmp[1] + dy[i];
-				if(0 <= nx && nx < n && 0<= ny && ny < n  && arr[nx][ny] == '1') {
-					q.add(new int[] {nx, ny});
-					arr[nx][ny] = '0';
-				}
-			}
-		}
-		return sum;
-	}
+    static int n;
+    static int[][] arr;
+    static boolean[][] visit;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        visit = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        int ans = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(arr[i][j] == 1 && !visit[i][j]){
+                    int cnt = dfs(i, j);
+                    list.add(cnt);
+                    ans++;
+                }
+            }
+        }
+        Collections.sort(list);
+        sb.append(ans + "\n");
+
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i) + "\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int dfs(int x, int y) {
+        if(x < 0 || x >= n || y < 0 || y >= n || arr[x][y] == 0 || visit[x][y]){
+            return 0;
+        }
+        visit[x][y] = true;
+        int cnt = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            cnt += dfs(nx, ny);
+        }
+        return cnt;
+    }
 }
