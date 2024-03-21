f95ee09fd9e4f245a9121984bbe4ffc639813824
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-03-21 09:17:49 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,57 +2,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int computer;
-	static int edge, cnt;
-	static boolean visit[];
-	static HashMap<Integer, List<Integer>> map =  new HashMap<>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		computer = Integer.parseInt(br.readLine());
-		edge = Integer.parseInt(br.readLine());
-		visit = new boolean[computer + 1]; 
-		cnt =  0;
-		for (int i = 0; i < computer; i++) {
-			map.put(i + 1, new ArrayList<>());
-		}
-		for (int i = 0; i < edge; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			map.get(a).add(b);
-			map.get(b).add(a);
-		}
-//		for (int i = 0; i < computer; i++) {
-//			System.out.println(map.get(i + 1).toString());
-//		}
-		bfs();
-		System.out.println(cnt);
-	}
+    static int n;
+    static ArrayList<Integer>[] arr;
+    static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        arr = new ArrayList[n + 1];
+        visit = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            arr[i] = new ArrayList<>();
+        }
 
-	private static void bfs() {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(1);
-		visit[1] = true;
-		while(!q.isEmpty()) {
-			List<Integer> tmp = map.get(q.poll());
-			for (int i = 0; i < tmp.size(); i++) {
-				if(!visit[tmp.get(i)]) {
-					visit[tmp.get(i)] = true;
-					q.add(tmp.get(i));
-					cnt++;
-				}
-			}
-		}
-		
-	}
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a].add(b);
+            arr[b].add(a);
+        }
+
+        dfs(1);
+        int ans = 0;
+        for (int i = 2; i <= n ; i++) {
+            if(visit[i]) ans++;
+        }
+        System.out.println(ans);
+    }
+
+    private static void dfs(int cur) {
+        if(visit[cur]) return;
+
+        visit[cur] = true;
+        ArrayList<Integer> list = arr[cur];
+        for (int i = 0; i < list.size(); i++) {
+            if(!visit[list.get(i)]){
+                dfs(list.get(i));
+            }
+        }
+    }
 }
