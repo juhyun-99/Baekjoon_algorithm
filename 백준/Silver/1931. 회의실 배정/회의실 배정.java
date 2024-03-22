0a845d6c16a9f285e2f1bd39f585987c18e68782
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-03-22 12:16:23 +0900

    [Silver I] Title: 회의실 배정, Time: 480 ms, Memory: 43020 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,67 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Meeting{
-	int start;
-	int end;
-	Meeting(int start , int end){
-		this.start = start;
-		this.end = end;
-	}
-	public int getStart() {
-		return start;
-	}
-	public void setStart(int start) {
-		this.start = start;
-	}
-	public int getEnd() {
-		return end;
-	}
-	public void setEnd(int end) {
-		this.end = end;
-	}
-	@Override
-	public String toString() {
-		return "Meeting [start=" + start + ", end=" + end + "]";
-	}
-	
+class Meeting implements Comparable<Meeting>{
+    int start;
+    int end;
+    Meeting(int start , int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(Meeting o) {
+        if(this.end != o.end){
+            return this.end - o.end;
+        }
+        return this.start - o.start;
+    }
 }
-public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		int n = Integer.parseInt(br.readLine());
-		List<Meeting> arr = new ArrayList<>();
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		arr.sort(Comparator.comparing(Meeting::getEnd).thenComparing(Meeting::getStart));
 
-		int start = arr.get(0).start;
-		int end = arr.get(0).end;
-		int cnt = 1;
-		for (int i = 1; i < n; i++) {
-			int tmpStart = arr.get(i).start;
-			int tmpEnd = arr.get(i).end;
-			
-			if(tmpStart >= end) {
-				cnt++;
-				end = tmpEnd;
-			}
-		}
-		
-		System.out.println(cnt);
-	}
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        List<Meeting> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+        Collections.sort(arr);
+
+        int end = arr.get(0).end;
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            int tmpStart = arr.get(i).start;
+            int tmpEnd = arr.get(i).end;
+
+            if(tmpStart >= end) {
+                cnt++;
+                end = tmpEnd;
+            }
+        }
+
+        System.out.println(cnt);
+    }
 }
