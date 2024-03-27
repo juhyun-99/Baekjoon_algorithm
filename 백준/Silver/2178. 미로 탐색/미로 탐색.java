b85e2b619a3b79faeb5541d4fd98f063b6f58d89
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-03-27 23:53:48 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12092 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,62 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
-    static class Point{
-        int x,y;
-        public Point(int x, int y) {
-            this.x=x;
-            this.y=y;
-        }
-    }
+class Point{
+    int x;
+    int y;
+    int time;
 
-    static int n,m;
+    public Point(int x, int y, int time){
+        this.x = x;
+        this.y = y;
+        this.time = time;
+    }
+}
+
+public class Main {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static int n, m;
+    static boolean[][] visit;
+    static int ans;
     static int[][] arr;
-    static int[][] dist;
-    static int[] dx = new int[] {1,-1,0,0};
-    static int[] dy = new int[] {0,0,1,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        st = new StringTokenizer(br.readLine());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new int[n][m];
-        dist = new int[n][m];
 
-        for(int i=0; i<n; i++) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            for(int j=0; j<m; j++) {
-                arr[i][j] = str.charAt(j)-'0';
-                if(i==0&&j==0) dist[i][j]=1;
+        visit = new boolean[n][m];
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = tmp.charAt(j) - '0';
             }
         }
 
-        Queue<Point> q = new LinkedList<Point>();
-        q.offer(new Point(0,0));
+        bfs();
+        System.out.println(ans);
+    }
 
-        while(!q.isEmpty()) {
-            Point cur = q.poll();
+    private static void bfs() {
+        Queue<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(0, 0, 1));
+        visit[0][0] = true;
 
-            for(int dir=0; dir<4; dir++) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
+        while(!queue.isEmpty()){
+            Point p = queue.poll();
+            if(p.x == n - 1 && p.y == m - 1){
+                ans = p.time;
+                break;
+            }
 
-                if(nx >=0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1 && dist[nx][ny] == 0){
-                    dist[nx][ny] = dist[cur.x][cur.y]+1;
-                    q.offer(new Point(nx,ny));
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(0 <= nx && nx < n && 0 <= ny && ny < m && arr[nx][ny] == 1 && !visit[nx][ny]){
+                    queue.add(new Point(nx, ny, p.time + 1));
+                    visit[nx][ny] = true;
                 }
             }
         }
-
-        System.out.println(dist[n-1][m-1]);
     }
+
 }
