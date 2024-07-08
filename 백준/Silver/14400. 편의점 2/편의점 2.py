29d250ef5f9616a0fe60a734d79de45ab85f36f2
juhyun <72910402+juhyun-99@users.noreply.github.com>
2024-07-08 14:16:06 +0900

    [Silver II] Title: 편의점 2, Time: 224 ms, Memory: 39216 KB -BaekjoonHub

--- 백준/Silver/14400. 편의점 2/편의점 2.py
+++ 백준/Silver/14400. 편의점 2/편의점 2.py
@@ -1,38 +1,23 @@
-#14400
 import sys
 input = sys.stdin.readline
 
-#완전탐색
-#x,y = -100만 ~100만
-# 201만 * 201만 = 시간초과
-
 n = int(input())
-x_arr = []  
-y_arr = []
+x, y = [], []
 
-for _ in range(n):
-    x,y = map(int,input().split())
-    x_arr.append(x)
-    y_arr.append(y)
+for i in range(n):
+    x1, y1 = map(int, input().split())
+    x.append(x1)
+    y.append(y1)
 
-#정렬
-x_arr.sort()
-y_arr.sort()
+x.sort()
+y.sort()
 
-if n % 2 == 0: #짝수개일 때 중간 값이 두개
-    ans1,ans2 = 0,0
-    for x in x_arr:
-        ans1 += abs(x - x_arr[n//2 - 1])
-        ans2 += abs(x - x_arr[n//2])
-    for y in y_arr:
-        ans1 += abs(y - y_arr[n//2 - 1])
-        ans2 += abs(y - y_arr[n//2])
-    ans = min(ans1,ans2)
+midX = x[n//2]
+midY = y[n//2]
 
-else: #홀수개일 때
-    ans = 0
-    for x in x_arr:
-        ans += abs(x - x_arr[n//2])
-    for y in y_arr:
-        ans += abs(y - y_arr[n//2])
+ans = 0
+for i in range(n):
+    ans += abs(x[i] - midX)
+    ans += abs(y[i] - midY)
+
 print(ans)
