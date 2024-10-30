860d4b73711299be6ba2a68a3dbf4a3f6a6e6a4f
nyang <72910402+nyang99@users.noreply.github.com>
2024-10-30 22:55:58 +0900

    [Gold III] Title: 파티, Time: 144 ms, Memory: 17340 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,82 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
 
 class Point implements Comparable<Point>{
-    int next;
-
-    int time;
-
-    public Point(int next, int time){
-        this.next = next;
-        this.time = time;
+    int to;
+    int dist;
+    public Point(int to, int dist){
+        this.to = to;
+        this.dist = dist;
     }
 
     @Override
     public int compareTo(Point o) {
-        return this.time - o.time;
+        return this.dist - o.dist;
     }
+
 }
 public class Main {
-    static int n, m, x, ans = Integer.MIN_VALUE;
-    static char[][] arr;
-    static ArrayList<ArrayList<Point>> list1, list2;
-
+    static ArrayList<Point>[] list, listReverse;
+    static int n, m, x;
+    static int[] dist, distReverse;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        list1 = new ArrayList<>();
-        list2 = new ArrayList<>();
-        arr = new char[n][m];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken()); //n명의 학생
+        m = Integer.parseInt(st.nextToken()); //m개의 마을
+        x = Integer.parseInt(st.nextToken()); //x번 마을에서 파티
+        list = new ArrayList[m + 1];
+        listReverse = new ArrayList[m + 1];
 
-        for (int i = 0; i <= n; i++) {
-            list1.add(new ArrayList<>());
-            list2.add(new ArrayList<>());
+        dist = new int[n + 1];
+        distReverse = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Arrays.fill(distReverse, Integer.MAX_VALUE);
+        dist[x] = 0;
+        distReverse[x] = 0;
+
+        for (int i = 1; i < m + 1; i++) {
+            list[i] = new ArrayList<>();
+            listReverse[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int t = Integer.parseInt(st.nextToken());
-            list1.get(a).add(new Point(b, t));
-            list2.get(b).add(new Point(a, t));
+            int dist = Integer.parseInt(st.nextToken());
+            list[a].add(new Point(b, dist));
+            listReverse[b].add(new Point(a, dist));
         }
 
-        int arr1[] = dijkstra(list1);
-        int arr2[] = dijkstra(list2);
+        dijk(x, list, dist);
+        dijk(x, listReverse, distReverse);
+        int answer = Integer.MIN_VALUE;
+//        System.out.println(Arrays.toString(dist));
+//        System.out.println(Arrays.toString(distReverse));
         for (int i = 1; i < n + 1; i++) {
-            ans = Math.max(ans, arr1[i] + arr2[i]);
+            answer = Math.max(answer, dist[i] + distReverse[i]);
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 
-    private static int[] dijkstra(List<ArrayList<Point>> list) {
+    private static void dijk(int x, ArrayList<Point>[] list, int[] dist) {
         PriorityQueue<Point> q = new PriorityQueue<>();
         q.add(new Point(x, 0));
-        int[] dist = new int[n + 1];
-        boolean visit[] = new boolean[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        dist[x] = 0;
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Point p = q.poll();
-            int tmp = p.next;
-            if(visit[tmp]) continue;
+            int now = p.to;
 
-            visit[tmp] = true;
-            ArrayList<Point> li = list.get(tmp);
-            for (int i = 0; i < li.size(); i++) {
-                if(dist[li.get(i).next] > dist[tmp] + li.get(i).time && !visit[li.get(i).next]){
-                    dist[li.get(i).next] = dist[tmp] + li.get(i).time;
-                    q.add(new Point(li.get(i).next, dist[li.get(i).next]));
+            for(Point next : list[now]){
+                if(dist[next.to] > dist[now] + next.dist){
+                    dist[next.to] = dist[now] + next.dist;
+                    q.add(new Point(next.to, dist[next.to]));
                 }
             }
         }
-        return dist;
     }
 }
